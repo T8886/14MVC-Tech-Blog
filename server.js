2eecd13b8f2d6ac6d0f8837f9e20df490edf554b
unknown <tetiana.lisina@gmail.comgit config --global user.name T8886>
2022-10-18 22:43:26 -0500

    resolved server issue, added CSS

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const hbs = exphbs.create({
-    helpers
-});
+const hbs = exphbs.create({helpers});
 
 const sess = {
     secret: process.env.DB_SECRET,
--- utils/auth.js
+++ utils/auth.js
@@ -1,7 +1,7 @@
 // Code is taken from previous activities
 const withAuth = (req, res, next) => {
     // Redirect user to login page if not logged in
-    if (!req.session.logged_in) {
+    if (!req.session.loggedIn) {
       res.redirect('/login');
     } else {
       next();
